a636459a033d4ec36b09b29f29122c497004b1f7
Tim <tsl1@cornell.edu>
2015-10-16 15:01:31 -0400

    Initial Commit

---
+++ Art Scene/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Art Scene
+//
+//  Created by Timothy Larkin on 10/16/15.
+//  Copyright © 2015 Timothy Larkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
